67d6ac9c365e8d3027770730677e774901d6e11f
joemardev <joemarb.dev@gmail.com>
2023-01-24 18:23:32 +0800

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,143 @@
+$(document).ready(() => {
+	InitCarousel();
+	GetGeneratedHeight();
+});
+
+
+
+const GetGeneratedHeight = () => {
+	const GEN_HEIGHT = $('body').height() - $('.ref-height').height() - ($('.mobile-navigation').height() + 20);
+	$('.tab-container').height(GEN_HEIGHT);
+	$('.list-container').height(GEN_HEIGHT);
+}
+
+const InitCarousel = () => {
+	// const DEF_WID = $('.header-wrapper').width();
+	// $('.owl-carousel').width(DEF_WID)
+	$('.owl-carousel').owlCarousel({
+		slideSpeed: 300,
+		paginationSpeed: 400,
+		items: 1,
+		loop: true,
+		dots: true,
+		autoplay: true,
+		autoplayTimeout: 4000,
+	})
+}
+
+
+$('.list-container').on('scroll', function () {
+
+	const CASINO_POS = $('#live-list').height();
+
+	const SLOT_POS = CASINO_POS + $('#slot-list').height();
+
+	const SPORT_POS = SLOT_POS + $('#sport-list').height();
+
+	const MINI_POS = SPORT_POS + $('#minigame-list').height();
+
+	const VIRTUAL_POS = MINI_POS + $('#virtuals-list').height();
+
+	const CURRENT_SCROLL = $(this).scrollTop() + 150;
+
+	if (CURRENT_SCROLL <= VIRTUAL_POS) {
+		$('.game-button').removeClass("active");
+		$('.virtual-tab').addClass("active");
+
+		if (CURRENT_SCROLL <= MINI_POS) {
+			$('.game-button').removeClass("active");
+			$('.mini-tab').addClass("active");
+
+			if (CURRENT_SCROLL <= SPORT_POS) {
+				$('.game-button').removeClass("active");
+				$('.sport-tab').addClass("active");
+
+				if (CURRENT_SCROLL <= SLOT_POS) {
+					$('.game-button').removeClass("active");
+					$('.slot-tab').addClass("active");
+
+					if (CURRENT_SCROLL <= CASINO_POS) {
+						$('.game-button').removeClass("active");
+						$('.casino-tab').addClass("active");
+					}
+				}
+			}
+		}
+	}
+
+});
+
+let onScroll = false;
+
+const DisableAutoScroll = () => {
+	if (onScroll) return true;
+	onScroll = true;
+	setTimeout(() => {
+		onScroll = false;
+	}, 500)
+
+}
+
+const ScrollToCasino = () => {
+	if (DisableAutoScroll()) return
+	$('.list-container').animate({
+		scrollTop: 0
+	}, 500);
+
+
+}
+
+const ScrollToSlot = () => {
+	if (DisableAutoScroll()) return
+	$('.list-container').animate({
+		scrollTop: $("#live-list").height()
+	}, 500);
+}
+
+const ScrollToSports = () => {
+	if (DisableAutoScroll()) return
+	const SPORT_POS = $("#live-list").height() + $("#slot-list").height() + 10;
+	$('.list-container').animate({
+		scrollTop: SPORT_POS
+	}, 500);
+}
+
+const ScrollToMiniGame = () => {
+	if (DisableAutoScroll()) return
+	const MINI_POS = $("#sport-list").height() + $("#live-list").height() + $("#slot-list").height() + 20;
+	$('.list-container').animate({
+		scrollTop: MINI_POS
+	}, 500);
+}
+
+const ScrollToVirtual = () => {
+	if (DisableAutoScroll()) return
+	const VIRTUAL_POS = $("#minigame-list").height() + $("#sport-list").height() + $("#live-list").height() + $("#slot-list").height() + 30;
+	$('.list-container').animate({
+		scrollTop: VIRTUAL_POS
+	}, 500);
+}
+
+$('.game-image').click(() => {
+	OpenLoginForm()
+})
+
+const OpenSideBar = () => {
+	$('.overlay').show();
+	$('.sidebar').show();
+}
+
+const CloseSideBar = () => {
+	$('.overlay').hide();
+	$('.sidebar').hide();
+}
+
+
+const OpenLoginForm = () => {
+	$('.LOGIN_WRAPPER').show();
+	CloseSideBar();
+}
+
+const CloseLoginForm = () => $('.LOGIN_WRAPPER').hide();
+
+
---
+++ assets/js/tailwind.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                "theme-1": '#fff',
+                "theme-2": '#333',
+                "theme-3": '#e42626',
+                "theme-4": '#bc9045'
+            }
+        }
+    }
+}
